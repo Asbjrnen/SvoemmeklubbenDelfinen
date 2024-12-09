1aabad4b213928715f592a5af7caf2917f136576
chrisHS03 <chso0005@stud.kea.dk>
2024-12-09 10:07:18 +0100

    test virker

--- test/MemberListTest.java
+++ test/MemberListTest.java
@@ -1,17 +1,12 @@
-package test;// IMPORTERER DE KLASSER, SOM JEG SKAL BRUGE TIL TESTEN, SOM ER I VORES SRC FOLDER. VI HENTER DEM FRA MODELS-PAKKEN I VORES PROJEKT
 import models.Member;
 import models.MemberList;
-// ORG.JUNIT.JUPITER ER ET HENTET BIBLIOTEK TIL TESTS, SOM SPECIFICERER DET ER VERSION 5 A JUnit VI BRUGER
-// API (Application Programming Interface) INDEHOLDER FUNKTIONER TIL TESTEN
-import org.junit.jupiter.api.Test;
-// IMPORT STATIC = BRUGER METODERNE DIREKTE, HVIS VI ALLEREDE HAR NOGLE EKSISTERENDE TING SOM FX ASSERTEQUALS
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MemberListTest
-{
-    @Test
-    void addMemberTest()
-    {
+class MemberListTest {
+
+    @org.junit.jupiter.api.Test
+    void addMember() {
         // OPRETTER NY LISTE MED ET NYT MEDLEM
         MemberList memberList = new MemberList();
         Member member = new Member("Test Name", 20, "Crawl", true, 1, true, false, "0:30", "1:00");
